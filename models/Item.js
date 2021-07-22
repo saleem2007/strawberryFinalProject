0dc17d9a23f5238259b16538c69d3dad4940d573
Ainouz <ainoumi@ad.vzwcorp.com>
2021-07-22 14:22:34 -0500

    adding TDD

--- models/Item.js
+++ models/Item.js
@@ -1,13 +1,13 @@
-const {Sequelize, DataTypes, Model} = require('sequelize')
-const {sequelize} = require('../db')
+const { Sequelize, DataTypes, Model } = require('sequelize')
+const { sequelize } = require('../db')
 
 
-class Item extends Model { }
+class Item extends Model {}
 
 Item.init({
     name: DataTypes.STRING,
     description: DataTypes.STRING,
-    price: DataTypes.float(5, 2),
+    price: DataTypes.DOUBLE(5, 2),
     image: DataTypes.STRING
 }, {
     sequelize,
--- models/index.test.js
+++ models/index.test.js
@@ -1,15 +1,74 @@
-const {Sauce} = require('./index');
-const {sequelize} = require('../db');
+const { sequelize } = require('../db');
+const { Category } = require('./Category');
+const { Inventory } = require('./Inventory');
+const { Item } = require('./Item');
+const { Order } = require('./Order');
+const { User } = require('./User');
 
-describe('Sauce Model', () => {
-	beforeAll(async () => {
-		await sequelize.sync({force: true})
-	});
 
-	test('can create a Sauce', async() => {
-		const testSauce = await Sauce.create({name : 'Cholula'})
-		expect(testSauce.name).toBe('Cholula')
-	})
+describe('testing 101', () => {
+    beforeAll(async() => {
+        await sequelize.sync({ force: true })
+    });
+
+    test('can create a item', async() => {
+        const testItem = await Item.create({
+            name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+            price: 109.95,
+            description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+        })
+        expect(testItem.name).toBe('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
+        expect(testItem.price).toBe(109.95)
+        expect(testItem.description).toBe("Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday")
+        expect(testItem.image).toBe("https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg")
+    })
+
+    test('can create a category', async() => {
+        const testCategory = await Category.create({
+            name: "men's shoes",
+        })
+
+        expect(testCategory.name).toBe("men's shoes")
+
+    })
+
+    test('can create a User', async() => {
+        const testUser = await User.create({
+            fullName: "michaelAinouz",
+            address: "101 center",
+            phone: "6822184301",
+            email: "don't call me"
+        })
+        expect(testUser.fullName).toBe("michaelAinouz")
+        expect(testUser.address).toBe("101 center")
+        expect(testUser.phone).toBe("6822184301")
+        expect(testUser.email).toBe("don't call me")
+    })
+
+    test('can create a Order', async() => {
+        const testOrder = await Order.create({
+            numItems: 21,
+            totalPrice: 105.03
+        })
+
+        expect(testOrder.numItems).toBe(21)
+        expect(testOrder.totalPrice).toBe(105.03)
+
+    })
+
+    test('can create a Inventory', async() => {
+        const testInv = await Inventory.create({
+            name: "verizon",
+            location: "irving 306",
+            size: 30090
+        })
+
+        expect(testInv.name).toBe("verizon")
+        expect(testInv.location).toBe("irving 306")
+        expect(testInv.size).toBe(30090)
+
+    })
 
 
 })
