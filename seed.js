48760d038835abdd574804efdfa84e5368dcd128
Raes <raessa@ad.vzwcorp.com>
2021-07-23 11:35:04 -0500

    played with database and able to populate

--- seed.js
+++ seed.js
@@ -2,19 +2,19 @@ const path = require('path');
 const fs = require('fs').promises;
 
 const {sequelize} = require('./db');
-const {Sauce} = require('./models');
+const {Item} = require('./models/Item');
 
 
 const seed = async () => {
 
     await sequelize.sync({ force: true });
 
-    const seedPath = path.join(__dirname, 'sauces.json'); // creates path to seed data
+    const seedPath = path.join(__dirname, 'items.json'); // creates path to seed data
     const buffer = await fs.readFile(seedPath); // reads json
     const {data} = JSON.parse(String(buffer)); //parses data
 
-    const dataPromises = data.map(item => Sauce.create(item))
-    await Promise.all(dataPromises)
+    const itemPromises = data.map(item => Item.create(item))
+    await Promise.all(itemPromises)
     console.log("db populated!")
 }
 
